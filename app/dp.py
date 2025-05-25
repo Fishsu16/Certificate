8906c6c5f37881e8ec8610523571dc2697d734cb
Fishsu16 <161776094+Fishsu16@users.noreply.github.com>
2025-05-25 21:33:03 +0800

    Update dp.py

--- app/dp.py
+++ app/dp.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=False, future=True)
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
