ea739ce29e0edbd5c89cdc278c7c913e1e7f0936
Fishsu16 <161776094+Fishsu16@users.noreply.github.com>
2025-05-26 03:55:07 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api import router as api_router
-from app.db import get_db
+from app.db import get_db, engine, Base
 
 app = FastAPI(title="Custom CA Certificate Server")
 
